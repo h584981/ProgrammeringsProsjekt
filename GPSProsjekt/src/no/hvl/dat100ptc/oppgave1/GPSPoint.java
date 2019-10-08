a7894f9ba383fe55402a879d29f3ba2d6861184a
Sebastian <Sebastian@158.37.193.117>
2019-10-08 13:49:10 +0200

    Test

--- GPSProsjekt/src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ GPSProsjekt/src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -8,7 +8,7 @@ public class GPSPoint {
 		
 	public GPSPoint(int time, double latitude, double longitude, double elevation) {
 
-		// TODO - konstruktur Test
+		// TODO - konstruktur
 
 		throw new UnsupportedOperationException(TODO.construtor("GPSPoint"));
 
